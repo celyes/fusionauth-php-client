31a0ce462ca617a0747e8e55d316f2f94dbcb6ed
Daniel DeGroff <daniel@fusionauth.io>
2021-04-30 16:04:43 -0600

    Working on email gate, sync up domain, deprecate old version of Verify email, take a request body instead.

--- src/FusionAuth/FusionAuthClient.php
+++ src/FusionAuth/FusionAuthClient.php
@@ -4909,11 +4909,33 @@ class FusionAuthClient
    *
    * @return ClientResponse The ClientResponse.
    * @throws \Exception
+   * @deprecated This method has been renamed to verifyEmailAddress and changed to take a JSON request body, use that method instead.
    */
   public function verifyEmail($verificationId)
   {
     return $this->startAnonymous()->uri("/api/user/verify-email")
         ->urlSegment($verificationId)
+        ->post()
+        ->go();
+  }
+
+  /**
+   * Confirms a user's email address. 
+   * 
+   * The request body will contain the verificationId. You may also be required to send a one-time use code based upon your configuration. When 
+   * the tenant is configured to gate a user until their email address is verified, this procedures requires two values instead of one. 
+   * The verificationId is a high entropy value and the one-time use code is a low entropy value that is easily entered in a user interactive form. The 
+   * two values together are able to confirm a user's email address and mark the user's email address as verified.
+   *
+   * @param array $request The request that contains the verificationId and optional one-time use code paired with the verificationId.
+   *
+   * @return ClientResponse The ClientResponse.
+   * @throws \Exception
+   */
+  public function verifyEmailAddress($request)
+  {
+    return $this->startAnonymous()->uri("/api/user/verify-email")
+        ->bodyHandler(new JSONBodyHandler($request))
         ->post()
         ->go();
   }
